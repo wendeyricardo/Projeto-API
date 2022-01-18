9d46cba03bf83c186b327a2593ed9475f710014a
wendey <wendeyricardo@gmail.com>
2022-01-18 20:56:58 -0300

    Aula 04 - Endpoint findById

---
+++ src/main/java/com/example/api/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.example.api.resources;
+
+import com.example.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+
+        return ResponseEntity.ok().body(new User(1,"wendey","wendeyricardo@gmail.com","123"));
+
+    }
+}
