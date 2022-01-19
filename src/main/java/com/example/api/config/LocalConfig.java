19e32d08d18b4b0071a161004435ba3f6f381770
wendey <wendeyricardo@gmail.com>
2022-01-18 22:21:56 -0300

    Aula 06 - Perfil de teste

---
+++ src/main/java/com/example/api/config/LocalConfig.java
@@ -0,0 +1,27 @@
+package com.example.api.config;
+
+import com.example.api.domain.User;
+import com.example.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB(){
+        User u1 =  new User(null, "wendey","wendeyricardo@gmail.com", "123");
+        User u2 =  new User(null, "ricardo","ricardowendey@gmail.com", "123");
+
+        repository.saveAll(List.of(u1,u2));
+    }
+
+}
