8812056fc0dac38ff03e396068dd72f224080140
wendey <wendeyricardo@gmail.com>
2022-01-18 21:37:21 -0300

    Aula 05 - UserService e UserRepository

--- src/main/java/com/example/api/resources/UserResource.java
+++ src/main/java/com/example/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.example.api.resources;
 
 import com.example.api.domain.User;
+import com.example.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1,"wendey","wendeyricardo@gmail.com","123"));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 }
