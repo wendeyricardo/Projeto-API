600a159dc442b385654e91a1b5a9aab323309aa1
wendey <wendeyricardo@gmail.com>
2022-01-20 22:38:11 -0300

    10. Aula 07 - Padrão DTO10. Aula 07 - Padrão DTO

--- src/main/java/com/example/api/resources/UserResource.java
+++ src/main/java/com/example/api/resources/UserResource.java
@@ -1,7 +1,9 @@
 package com.example.api.resources;
 
 import com.example.api.domain.User;
+import com.example.api.domain.dto.UserDTO;
 import com.example.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
 
     }
 }
