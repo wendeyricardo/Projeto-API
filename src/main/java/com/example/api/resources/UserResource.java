21473e1381b7a0ca82d6a37a1c6cf0745a968650
wendey <wendeyricardo@gmail.com>
2022-01-21 10:17:19 -0300

    15. Aula 12 - Endpoint delete

--- src/main/java/com/example/api/resources/UserResource.java
+++ src/main/java/com/example/api/resources/UserResource.java
@@ -17,6 +17,8 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    public static final String ID = "{id}";
+
     @Autowired
     private ModelMapper mapper;
 
@@ -50,6 +52,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
         obj.setId(id);
         return  ResponseEntity.ok().body((mapper.map(service.update(obj), UserDTO.class)));
+
+    }
+
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return  ResponseEntity.noContent().build();
 
     }
 }
