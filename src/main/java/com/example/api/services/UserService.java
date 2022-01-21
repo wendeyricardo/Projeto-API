2daad1903e18074ebbd7d81ba396af53f9a1b454
wendey <wendeyricardo@gmail.com>
2022-01-20 23:27:16 -0300

    11. Aula 08 - Endpoint findAll

--- src/main/java/com/example/api/services/UserService.java
+++ src/main/java/com/example/api/services/UserService.java
@@ -2,7 +2,10 @@ package com.example.api.services;
 
 import com.example.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
