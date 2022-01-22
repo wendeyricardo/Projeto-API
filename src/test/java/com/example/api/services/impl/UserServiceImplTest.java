5dd6f60db14b7fe7139c358f3ff260aecdab3d3c
wendey <wendeyricardo@gmail.com>
2022-01-22 12:50:00 -0300

    18. Aula 15 - Teste UserService objeto não encontrado

--- src/test/java/com/example/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/example/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.example.api.services.impl;
 import com.example.api.domain.User;
 import com.example.api.domain.dto.UserDTO;
 import com.example.api.repositories.UserRepository;
+import com.example.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -59,6 +60,18 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail()
         );
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundExcepction(){
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+
+        try {
+            service.findById(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto não encontrado",ex.getMessage());
+        }
     }
 
     @Test
