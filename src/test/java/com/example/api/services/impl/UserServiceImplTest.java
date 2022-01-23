4a551b166e27b2d8be3038331b3540cee79755fa
wendey <wendeyricardo@gmail.com>
2022-01-22 23:56:01 -0300

    21. Aula 18 - Teste UserService update

--- src/test/java/com/example/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/example/api/services/impl/UserServiceImplTest.java
@@ -113,6 +113,7 @@ class UserServiceImplTest {
 
         try{
             optionalUser.get().setId(2);
+            service.create(userDTO);
         }catch (Exception ex){
             assertEquals(DataIntegratyViolationException.class, ex.getClass());
             assertEquals("E-mail já cadastrado no sistema",ex.getMessage());
@@ -120,7 +121,30 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSucess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID,response.getId());
+        assertEquals(NAME,response.getName());
+        assertEquals(EMAIL,response.getEmail());
+        assertEquals(PASSWORD,response.getPassword());
+    }
+
+    @Test
+    void whenUpdateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try{
+            optionalUser.get().setId(2);
+            service.create(userDTO);
+        }catch (Exception ex){
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail já cadastrado no sistema",ex.getMessage());
+        }
     }
 
     @Test
