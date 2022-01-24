2e545bbc72966c82b6518b2705a60ce8aafe9d10
wendey <wendeyricardo@gmail.com>
2022-01-24 09:37:41 -0300

    27. Aula 24 - Teste UserResource delete

--- src/test/java/com/example/api/resources/UserResourceTest.java
+++ src/test/java/com/example/api/resources/UserResourceTest.java
@@ -117,7 +117,15 @@ class UserResourceTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteThenReturnSuccess() {
+        doNothing().when(service).delete(anyInt());
+
+        ResponseEntity<UserDTO> response = resource.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        verify(service, times(1)).delete(anyInt());
     }
 
     private void startUser() {
