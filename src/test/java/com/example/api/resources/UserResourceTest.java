fc3efa8281562efba7c2c0c161b04124caae6111
wendey <wendeyricardo@gmail.com>
2022-01-24 10:35:14 -0300

    30. Aula 27 - Bugfix

--- src/test/java/com/example/api/resources/UserResourceTest.java
+++ src/test/java/com/example/api/resources/UserResourceTest.java
@@ -31,8 +31,8 @@ class UserResourceTest {
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
 
 
-    private User user;
-    private UserDTO userDTO;
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
 
     @InjectMocks
     private UserResource resource;
